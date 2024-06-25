94d4e4aca6d2c5d264d2d3badf2c0db0c075319f
BilalHussain <hellobilalhussain@gmail.com>
2024-06-25 12:05:58 +0500

    Create Meal Deals Page

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,10 +1,32 @@
-import Link from "next/link";
+import Image from "next/image";
+
+import { getMeal } from "@/lib/meals";
+import classes from "./page.module.css";
 
 export default function MealDetails({ params }) {
+  const meal = getMeal(params.mealSlug);
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
   return (
     <>
-      <p>{params.mealSlug} Meal Details</p>
-      <Link href="/meals">Back</Link>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main className={classes.main}>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
     </>
   );
 }
